3f4f6ba82926fbacc37fa739f5929f965a6a10e6
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2022-04-06 11:57:25 +0530

    Vidly backend app with all 4 CRUD operation and a minimum Input Validation

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const Joi = require("joi");
+const app = express();
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Thriller" },
+  { id: 3, name: "Romance" },
+];
+
+const schema = Joi.object({
+  name: Joi.string().min(3).required(),
+});
+const { error } = schema.validate({ name: "22" });
+
+console.log(error.details[0].message);
+// Create
+app.post("/api/genres", (req, res) => {
+  console.log(req.body.name);
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.status(200).send(genre);
+});
+
+//   Read All
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+//Read Particular
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id == req.params.id);
+
+  //The Below commented Codes is doing exactly what a previous single line Code DOs
+  //   const genre = genres.find((c) => {
+  //     if (c.id == req.params.id) {
+  //       console.log("C .id : ", c.id);
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+
+  //   );
+
+  if (!genre)
+    return res
+      .status(404)
+      .send(`The Given Id (${req.params.id}) was not Found.!`);
+
+  res.send(genre);
+});
+
+//Update
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id == req.params.id);
+
+  if (!genre)
+    return res
+      .status(404)
+      .send(`The Given Id (${req.params.id}) was not Found.!`);
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.status(202).send(genre);
+});
+
+// Delete
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id == req.params.id);
+
+  if (!genre)
+    return res
+      .status(404)
+      .send(`The Given Id (${req.params.id}) was not Found.!`);
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res
+    .status(200)
+    .send(`The given id(${req.params.id}) has deleted Successfully`);
+});
+
+app.listen(3000, () => console.log("Listening to the Port 3000"));
