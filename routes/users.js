2e6ee769f76ec23a21b98fdea7d5c3b9e1369b3a
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2022-05-13 11:07:59 +0530

    Password hashing and Save the Hased password in MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -11,18 +13,15 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  console.log("user: " + user);
   if (user)
     return res.status(400).send(`User Already Registered.! "${user.email}"`);
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
- 
-  res.status(200).send(user);
+
+  res.status(200).send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
