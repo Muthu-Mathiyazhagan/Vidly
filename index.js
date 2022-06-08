b4fd6ff5880b64ca206828e140ecbbec4d92717e
Muthu <muthu@divum.in>
2022-06-08 15:58:57 +0530

    Handling Uncaught Exceptions and Unhandled Rejection with Inbuild process.on

--- index.js
+++ index.js
@@ -29,24 +29,24 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use(error);
 
-winston.add(winston.transports.File,{filename : 'logfile.log'});
-winston.add(winston.transports.MongoDB,{db: config.get('dbUri')});
+winston.add(winston.transports.File, { filename: 'logfile.log' });
+winston.add(winston.transports.MongoDB, { db: config.get('dbUri') });
 
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   defaultMeta: { service: 'user-service' },
-//   transports: [
-//     //
-//     // - Write all logs with importance level of `error` or less to `error.log`
-//     // - Write all logs with importance level of `info` or less to `combined.log`
-//     //
-//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-//     new winston.transports.File({ filename: 'combined.log' }),
-//   ],
-// });
+process.on('uncaughtException', (ex) => {
+  console.log("WE GOT AN UNCAUGHT EXCEPTION");
+  winston.error(ex.message, ex);
+});
 
-// winston.add(new winston.transports.MongoDB({db: config.get('dbUri')}));
+
+process.on('unhandledRejection', (ex) => {
+  console.log("WE GOT AN UNHANDLED REJECTION");
+  winston.error(ex.message, ex);
+});
+
+
+
+
+// throw new Error('Something Failed');
 
 
 if (!process.env.vidly_jwtPrivateKey) {
